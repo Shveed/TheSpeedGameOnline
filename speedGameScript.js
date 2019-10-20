05dcd949367eb563ed4038dee9dd758c68a028b9
Иван <vania.shvedkov@yandex.ru>
2019-10-20 23:33:39 +0300

    Добавлена рамка по нажатию клавиши

--- speedGameScript.js
+++ speedGameScript.js
@@ -1,3 +1,7 @@
 function startGame(){
-	
+	var buttonToHide = document.getElementById("startButton");
+	var gameArea = document.getElementById("gameArea");
+	buttonToHide.className = "hide";
+	gameArea.className = "playingArea";
+
 }
